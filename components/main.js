fcb9390e540c4aa82c69cb20c355bbfcb6d22930
Rodrigo Thauã Lima Barroso <rbarroso@sesc-ro.esplanada>
2021-09-10 16:04:04 -0400

    inclusao de janela e porta

--- components/main.js
+++ components/main.js
@@ -2,12 +2,12 @@ function calcula(){
     var larg = document.getElementById('larguraparede').value
     var alt = document.getElementById('alturaparede').value
     var janela = document.getElementById('qtdjanelas').value
-    var portas = document.getElementById('qtdportas').value
+    var porta = document.getElementById('qtdportas').value
 
     janela = 2 * 1.20
     porta = 0.8 * 1.90
 
-    area = larg * alt
+    area = larg * alt - janela - porta
     
     alert('Sua parede tem a dimensão de ' + larg+'x'+alt + ' e sua área é de ' + area +'m²')
 
