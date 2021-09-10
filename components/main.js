2eb756c83da3706f3fa2a22099dc894125df044d
Rodrigo Thauã <rodrigothaua@gmail.com>
2021-09-10 00:53:31 -0400

    first commit

---
+++ components/main.js
@@ -0,0 +1,17 @@
+function calcula(){
+    var larg = document.getElementById('larguraparede').value
+    var alt = document.getElementById('alturaparede').value
+    var janela = document.getElementById('qtdjanelas').value
+    var portas = document.getElementById('qtdportas').value
+
+    janela = 2 * 1.20
+    porta = 0.8 * 1.90
+
+    area = larg * alt
+    
+    alert('Sua parede tem a dimensão de ' + larg+'x'+alt + ' e sua área é de ' + area +'m²')
+
+    tinta = area / 2
+
+    alert('Para pintar essa parede, você precisará de ' + tinta + 'L de tinta')
+}
